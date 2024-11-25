d956372cb4e36b3beeae09ae037590bd435b7d98
aegjoyce <47081267+aegjoyce@users.noreply.github.com>
2024-11-25 20:14:05 +0000

    Revert "add web app install button"
    This reverts commit 17bb29e2972e184f9925076ad954e59a32afd5bf.

--- script.js
+++ script.js
@@ -337,34 +337,6 @@ function formatValue(value, unit = '') {
     return unit ? `${formattedValue} ${unit}` : formattedValue;
 }
 
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent the mini-infobar from appearing on mobile
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI to notify the user they can install the PWA
-    const installButton = document.getElementById('install-button');
-    installButton.style.display = 'block';
-
-    installButton.addEventListener('click', () => {
-        // Hide the install button
-        installButton.style.display = 'none';
-        // Show the install prompt
-        deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice.then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the install prompt');
-            } else {
-                console.log('User dismissed the install prompt');
-            }
-            deferredPrompt = null;
-        });
-    });
-});
-
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker.register('/service-worker.js').then(registration => {
