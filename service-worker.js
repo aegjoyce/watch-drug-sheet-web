1f41dc27b49ac19960d00415845494b178f710fe
aegjoyce <47081267+aegjoyce@users.noreply.github.com>
2025-03-10 19:53:21 +0000

    test

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'v29'; // Increment this every time you update files
+const CACHE_NAME = 'v30'; // Increment this every time you update files
 const urlsToCache = [
   './', // Cache the root URL
   './index.html', // Ensure index.html is cached properly
