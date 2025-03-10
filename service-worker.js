9063c14d0af9e5eae754be05ec956d1a27a106f0
aegjoyce <47081267+aegjoyce@users.noreply.github.com>
2025-03-10 14:09:17 +0000

    final test

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'v21'; // Increment this manually to force updates
+const CACHE_NAME = 'v22'; // Increment this manually to force updates
 const urlsToCache = [
   './',
   './index.html',
